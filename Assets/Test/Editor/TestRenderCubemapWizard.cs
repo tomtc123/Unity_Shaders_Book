1e16a096858a9a389a9a2e9f7a41a469037d394a
tangcheng <997063802@qq.com>
2018-12-26 20:15:41 +0800

    chapter 9

---
+++ Assets/Test/Editor/TestRenderCubemapWizard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestRenderCubemapWizard : ScriptableWizard {
+
+	public Transform renderFromPosition;
+	public Cubemap cubemap;
+
+	void OnWizardUpdate() {
+		string helpString = "Select transform to render from and cubemap to render into";
+		bool isValid = (renderFromPosition != null) && (cubemap != null);
+	}
+
+	void OnWizardCreate() {
+		GameObject go = new GameObject("CubemapCamera");
+		go.AddComponent<Camera>();
+
+		go.transform.position = renderFromPosition.position;
+		go.transform.rotation = Quaternion.identity;
+
+		go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+		DestroyImmediate(go);
+	}
+
+	[MenuItem("GameObject/Render/Test Rendr into Cubemap")]
+	static void RenderCubemap() {
+		TestRenderCubemapWizard.DisplayWizard<TestRenderCubemapWizard>("Render Cubemap", "Render!");
+	}
+}
